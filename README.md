48bd90b2bd4834f2dae9f298a4423e2a78526f43
Celio Oliveira <celio@Celios-MacBook-Pro.local>
2021-01-27 06:23:12 -0500

    Updates: - ReadMe, - Script, - Input and Output folders, - License, - references.bib

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# MCI_Toronto2019
+Shared By: TorontoPoliceService
+--------
+
+Data Source: services.arcgis.com
+
+=======
+
+INF2178AssignmentOne_Major Crime Indicators_Toronto2019
+
+This repo includes all Major Crime Indicators (MCI) 2014 to 2019 occurrences by reported date and related offenses. Major Crime Indicators (MCI) Dashboard.
+
+The dataset can be downloaded here: [Major Crime Indicators - Toronto 2014-1019](https://opendata.arcgis.com/datasets/56a0d46ae5f141269f2598a8c07e25c1_0).
+
+The Major Crime Indicators categories are Assault, Break and Enter, Auto Theft, Robbery and Theft Over (Excludes Sexual Assaults).
+
+By accessing these datasets, the user agrees to full acknowledgement of the Open Government License - Ontario.
+
+In accordance with the Municipal Freedom of Information and Protection of Privacy Act, the Toronto Police Service has taken the necessary measures to protect the privacy of individuals involved in the reported occurrences. No personal information related to any of the parties involved in the occurrence will be released as open data.
+
+The location of crime occurrences have been deliberately offset to the nearest road intersection node to protect the privacy of parties involved in the occurrence. All location data must be considered as an approximate location of the occurrence and users are advised not to interpret any of these locations as related to a specific address or individual.
+
+NOTE: Due to the offset of occurrence location, the numbers by Division and Neighborhood may not reflect the exact count of occurrences reported within these geographies. Therefore, the Toronto Police Service does not guarantee the accuracy, completeness, timeliness of the data and it should not be compared to any other source of crime data.
+
+The reported crime dataset is intended to provide communities with information regarding public safety and awareness. The data supplied to the Toronto Police Service by the reporting parties is preliminary and may not have been fully verified.
---
+++ scripts/MCI_Toronto2019.R
@@ -0,0 +1,183 @@
+---
+#   Title: Toronto Major Crime Indicators
+# Subtitle: Crime by Neighborhoods
+# Purpose: Use Toronto Police Service Public Safety Data Portal to get crime data from
+# Toronto by Major Crime Indicators
+# Author: Celio Oliveira
+# Contact: oliveira.celior@gmail.com
+# Date: r format(Sys.time(), '%d %B %Y')
+# Pre-requisites: None
+# Output:bookdown::pdf_document2:toc: yes
+# thanks: 'Code and data are available at: https://github.com/CROliveira/INF2178_AssignmentOne'
+# bibliography: references.bib
+# output:
+#   pdf_document: default
+# html_document:
+#   df_print: paged
+---
+  
+  
+# MAJOR CRIME INDICATORS IN TORONTO 2019
+
+
+
+## Setting up the environment
+
+# check.packages function: install and load multiple R packages.
+# Check to see if packages are installed. Install them if they are not, then load them into the R session.
+check.packages <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+# Passing the libraries call to the check.packages function
+packages<-c("tidyverse", "here", "dplyr", "ggplot2",  "ggmap")
+check.packages(packages) # Return TRUE if packages are installed, FALSE otherwise
+
+here::here()
+
+# Introduction
+
+# The Data
+
+# library(opendatatoronto)
+# 
+# MciDataDescription <-
+#   opendatatoronto::search_packages("Major Crime Indicators") %>%
+#   opendatatoronto::list_package_resources() %>%
+#   filter(name == "data_dictionary.csv") %>%
+#   select(id) %>%
+#   opendatatoronto::get_resource()
+
+# package <- show_package("247788f6-ca20-42e8-b00f-894ac43053e5")
+# write.csv(here::here("inputs/data/MciDataDescription.csv"))
+# View(MciDataDescription)
+
+# url <- "https://opendata.arcgis.com/datasets/56a0d46ae5f141269f2598a8c07e25c1_0.geojson"
+
+# outputFile <- "/Users/celio/Desktop/UofT/Course load/4th_Term/INF2178H - Experimental Design for DS/Assignment/INF2178_AssignmentOne"
+
+# data <- read_csv(here("inputs/data/MCI_2014_to_2019.csv"))
+# 
+# drop <- c("X","Y")
+# data = data[,!(names(data) %in% drop)] # Dropping columns X, Y.
+
+# fileteredMCI <- filter(data, occurrenceyear == "2019")
+# write.csv(here::here("inputs/data/fileteredMCI.csv"))
+
+
+
+## Data Description
+
+
+mci <- read_csv(here("inputs/data/fileteredMCI.csv"))
+# head(mci) # display first six rows
+# names(mci) # column names
+
+# mci_type <- table(mci$MCI, mci$occurrencedate) # Distinct value count for MCI column
+
+ggplot(mci, aes(y=MCI, fill=occurrencemonth)) +
+  geom_bar(stat="count", position = position_dodge2(preserve = "single")) +
+  theme(axis.title.y = element_blank(),
+        axis.title.x = element_blank()) +
+  labs(title="Major Crimes Indicator TORONTO 2019", caption = " Assault:19600; Auto-theft:5137; Break and Enter:8278; Robbery:3408; Theft-over:1251 ")
+
+dim(mci) # Get the data size
+
+object.size(mci) # Get the memory size of the data
+
+#There are many summary statistics available in R; Psych function provides the ones most useful for scale construction and item analysis in classic [Psychometrics](https://www.rdocumentation.org/packages/psych/versions/2.0.12/topics/describe) that brings a Summary Statistics about the non-categorical features.
+
+
+# describe(mci, omit = TRUE, fast = NULL)
+
+
+# Research Questions
+
+## At what time most crimes happen in Toronto?
+
+
+hour_crime <- summarise(group_by(mci, occurrencehour, MCI), n=n())
+ggplot(hour_crime, aes(x=occurrencehour, y=n, color=MCI)) +
+  geom_line(size=1) +
+  ggtitle(' Hourly Crimes in Toronto 2019') +
+  ylab('# Occurrences') +
+  xlab('24 hour clock') +
+  theme_bw() + labs(title="Hourly Crime Types in Toronto 2019") +
+  theme(plot.title = element_text(size = 16),
+        axis.title = element_text(size = 12, face = "bold"))
+
+
+## What is the relation between time of the day and crime type?
+
+# indicator_group <- group_by(mci, MCI)
+# crime_by_indicator <- summarise(indicator_group, n=n())
+# crime_by_indicator <- crime_by_indicator[order(crime_by_indicator$n, decreasing = TRUE),]
+# ggplot(aes(x = reorder(MCI, n), y = n), data = crime_by_indicator) +
+#   geom_bar(stat = 'identity', width = 0.5) +
+#   geom_text(aes(label = n), stat = 'identity', data = crime_by_indicator, hjust = -0.1, size = 2.3) +
+#   coord_flip() +
+#   xlab('Major Crime Indicators') +
+#   ylab('Number of Occurrences') +
+#   ggtitle('Major Crime Indicators Toronto 2016') +
+#   theme_bw() +
+#   theme(plot.title = element_text(size = 16),
+#         axis.title = element_text(size = 12, face = "bold"))
+
+
+
+## What are the top 10 Assault subgroups
+
+AssaultSubGroup <- summarise(group_by(mci[mci$MCI == 'Assault', ], offence), n=n())[order(summarise(group_by(mci[mci$MCI == 'Assault', ], offence), n=n())$n, decreasing = TRUE), ]
+
+AssaultSubGroup <- AssaultSubGroup %>% slice_max(offence, n = 10) # Top 10 values
+
+ggplot(aes(x = reorder(offence, n), y = n), data = AssaultSubGroup) +
+  geom_bar(stat = 'identity', width = 0.6) +
+  geom_text(aes(label = n), stat = 'identity', data = AssaultSubGroup, hjust = -0.1, size = 2) +
+  coord_flip() +
+  xlab('Types of Assault') +
+  ylab('# Occurrences') +
+  ggtitle(' Offences in Toronto 2019') +
+  theme_bw() +
+  theme(plot.title = element_text(size = 16),
+        axis.title = element_text(size = 12, face = "bold"),
+        axis.title.y = element_blank())
+
+
+
+
+## Where in the city these crimes are most likely to happen?
+
+
+crime_location <- summarise(group_by(mci, Neighbourhood), n=n())[order(summarise(group_by(mci, Neighbourhood), n=n())$n, decreasing = TRUE), ]
+top25_location <- head(summarise(group_by(mci, Neighbourhood), n=n()), 25)
+ggplot(aes(x = reorder(Neighbourhood, n), y = n), data = top25_location) +
+  geom_bar(stat = 'identity', width = 0.5) +
+  geom_text(aes(label = n), stat = 'identity', data = top25_location, hjust = -0.1, size = 2.5) +
+  coord_flip() +
+  ggtitle('Top 25 Neighbourhood by crime type') +
+  theme(plot.title = element_text(size = 10),
+        axis.title = element_text(size = 8),
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank())
+
+
+map_data <- data.frame(mci$MCI, mci$Lat, mci$Long)
+colnames(map_data) <- c('Crimes', 'Lat', 'Lon')
+sbbox <- make_bbox(lon = mci$Long, lat = mci$Lat, f = 0.01)
+my_map <- get_map(location = sbbox, maptype = "roadmap", scale = 2, color="bw", zoom = 9)
+ggmap(my_map) +
+  geom_point(data=map_data, aes(x = mci$Long, y = mci$Lat, color = "#27AE60"), 
+             size = 0.5, alpha = 0.05) +
+  ggtitle("       Toronto's Crime Map 2019") +
+  guides(color=FALSE) +
+  facet_wrap(~ Crimes, nrow = 2) +
+  theme(axis.line = element_blank(), axis.text = element_blank(),
+        axis.ticks = element_blank(), axis.title.x = element_blank(), 
+        axis.title.y = element_blank(), plot.margin = unit(c(0, 0, -1, -1), 'lines'))
+
+
+
